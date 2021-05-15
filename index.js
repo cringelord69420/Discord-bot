43a42000b5d5906a25d9b0d728454ece0db50b82
cringelord <cloverkitty8@gmail.com>
2021-05-15 13:53:19 -0400

    test

---
+++ index.js
@@ -0,0 +1,62 @@
+const Discord = require('discord.js');
+const client = new Discord.Client()0
+
+const jokes = [
+    "whats the best thing about switzerland? idk but the flag is a big plus",
+    "this is such a funny video!https://www.youtube.com/watch?v=dQw4w9WgXcQ ",
+    "why do actors tell you to break a leg?, because every play has a cast ",
+]
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+  if (msg.content === 'bots attack!') {
+    msg.reply('"pew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pewpew pew pew');
+  }
+  if (msg.content === 'bot tell me a joke') {
+    msg.reply(jokes[Math.random() * 3 | 0]);
+  }
+  if (msg.content === 'bot rickroll everyone') {
+    msg.reply('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  }
+  if (msg.content === 'bot rickroll everyone') {
+    msg.reply('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  }
+  if (msg.content === 'bot rickroll everyone') {
+    msg.reply('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  }
+  if (msg.content === 'bot rickroll everyone') {
+    msg.reply('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  }
+  if (msg.content === 'bot rickroll everyone') {
+    msg.reply('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  }
+  if (msg.content === 'bot rickroll everyone') {
+    msg.reply('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  }
+  if (msg.content === 'bot rickroll everyone') {
+    msg.reply('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  }
+  if (msg.content === 'bot rickroll everyone') {
+    msg.reply('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  }
+  if (msg.content === 'bot rickroll everyone') {
+    msg.reply('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  }
+  if (msg.content === 'bot rickroll everyone') {
+    msg.reply('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  }
+  if (msg.content === 'bot rickroll everyone') {
+    msg.reply('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  }
+  if (msg.content === 'bot rickroll everyone') {
+    msg.reply('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  }
+  if (msg.content === 'bot rickroll everyone') {
+    msg.reply('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  }
+
+});
+
+client.login('ODM4MTAzNzc0ODQ5MjY5ODIx.YI2O9w.uHxyhGoGlyHk98NLmxrTPXLDVpY');
